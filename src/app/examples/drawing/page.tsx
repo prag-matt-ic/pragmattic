2ba17e74ae95db99e3051cc262e42930a7e1a9c3
Matthew <matthew@loopspeed.co.uk>
2025-01-12 22:20:31 +0000

    Add WIP on Drawing app

---
+++ src/app/examples/drawing/page.tsx
@@ -0,0 +1,9 @@
+import DrawingCanvas from '@/components/examples/drawing/DrawingApp'
+
+export default function DrawingAppPage() {
+  return (
+    <main className="h-lvh w-full bg-black">
+      <DrawingCanvas />
+    </main>
+  )
+}
