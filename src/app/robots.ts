daf7f50e0c0a44fe8a14bdf4ae745031f0229288
Matthew <matthew@loopspeed.co.uk>
2025-01-21 21:48:31 +0000

    Create robots.ts

---
+++ src/app/robots.ts
@@ -0,0 +1,11 @@
+import { MetadataRoute } from 'next'
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+    },
+    sitemap: 'https://pragmattic.vercel.app/sitemap.xml',
+  }
+}
